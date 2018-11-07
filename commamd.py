c137072c2b8cf4df1fa2a790618addd758f656de
j13tw <j13tw@yahoo.com.tw>
2018-11-07 18:38:34 +0800

    add command use function

--- commamd.py
+++ commamd.py
@@ -8,7 +8,9 @@ NC = Network_config.Net_config()
 NTP = Network_config.Ntp_config()
 TC = Network_config.Time_config()
 FS = Network_config.File_search()
+WG = Network_config.Watchdog_config()
 GPS = Network_config.Gps_time()
+RB = Network_config.Reboot_system()
 
 # inner Ethernet card
 NC.eth0_dhcp()
@@ -33,17 +35,42 @@ os.sys('lsusb | grep "Realtek" | cut -c16,17,18')
 
 # NTP Server time check 
 NTP.ntp_set('TIME.google.com')
+--> "OK" / "ERROR"
 
 # date / time control for user By hands
-TC.date_set('2018', '07', '31') 
+TC.get_now()
+--> “2018-01-31 10:20:30" 
+TC.date_set('2018', '07', '31')
+--> "OK" 
 TC.time_set('20', '10', '30')
+--> "OK"
 
 # get on path ".ini" file list
 FS.ini_list() 
 -> ["ABC.ini", "BCD.ini", "CDE.ini"]
+
+# config watchdog monitor file
+WG.watchdog_status()
+--> ['cpu_short_load_status', 'cpu_short_load', 'cpu_middle_load_status', 'cpu_middle_load', 'cpu_long_load_status', 'cpu_long_load']
+WG.set_cpu_load_short('45')
+--> "OK" / "ERROR"
+WG.set_cpu_load_middle('40')
+--> "OK" / "ERROR"
+WG.set_cpu_load_long('30')
+--> "OK" / "ERROR"
+WG.set_cpu_temperature('80')
+--> "OK" / "ERROR"
+WG.remove_cpu_load_short()
+--> "OK" / "ERROR"
+WG.remove_cpu_load_middle()
+--> "OK" / "ERROR"
+WG.remove_cpu_load_long()
+--> "OK" / "ERROR"
 
 # use gps to fix your device date & time
 GPS.get_time()
 -> ERROR is GPS-Sensor not found
 -> OK is set System time OK 
 
+# restart system command
+RB.reboot()
